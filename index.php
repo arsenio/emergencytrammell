15e8252fe01cc5c0c2c27cb4eef6fa7950ac1341
Arsenio Santos <arsenio@gmail.com>
2011-03-12 23:18:34 +0000

    Readded Google Analytics

--- index.php
+++ index.php
@@ -172,3 +172,12 @@ $browser = new Browser();
 
 </body>
 </html>
+<script type="text/javascript">
+var gaJsHost = (("https:" == document.location.protocol) ? "https://ssl." : "http://www.");
+document.write(unescape("%3Cscript src='" + gaJsHost + "google-analytics.com/ga.js' type='text/javascript'%3E%3C/script%3E"));
+</script>
+<script type="text/javascript">
+try {
+var pageTracker = _gat._getTracker("UA-7614079-1");
+pageTracker._trackPageview();
+} catch(err) {}</script>
