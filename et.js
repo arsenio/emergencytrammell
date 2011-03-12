1ed0fbfa8b3c7b9e395d2fbcaf09344f29e54415
ryanmarshall <rmarsh000@gmail.com>
2011-03-12 17:44:36 -0600

    fixed random to add #6

--- et.js
+++ et.js
@@ -75,7 +75,7 @@ dojo.addOnLoad(function() {
 });
 
 function random_file() {
-    num = 1 + (Math.floor(Math.random()*5));
+    num = 1 + (Math.floor(Math.random()*6));
     //num = 6;
     return 'etsound' + num;
 }
